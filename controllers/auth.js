e747849b37d6690e8b8f867c908bdd544b84b904
veronicadev <viarengoveronica@gmail.com>
2019-09-04 21:17:35 +0200

    Auth token: reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('./../models/user');
 const bcryptjs = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth:{
@@ -28,6 +29,56 @@ exports.getLogin = (req, res, next) => {
         errorMessage: message,
         path: "/login"
     });
+};
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if(message.length>0){
+        message= message[0];
+    }else{
+        message = null;
+    }
+    res.render("shop/reset", {
+        docTitle: "Reset password",
+        errorMessage: message,
+        path: "/reset"
+    });
+};
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (error, buffer)=>{
+        if(error){
+            console.log(error);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({email: req.body.email})
+            .then(user=>{
+                if(!user){
+                    req.flash('error', 'No user with that mail found.');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExp = Date.now() + 3600000;
+                user.save();
+            })
+            .then((result)=>{
+                res.redirect('/login');
+                return transporter.sendMail({
+                    to:req.body.email,
+                    from: 'info@octoshop.com',
+                    subject: 'Octoshop - password reset',
+                    html: `
+                    <h1>
+                        You requested a password reset. Click the link below to set a new password
+                    </h1>
+                    <a href="${req.baseUrl}/reset-password/${token}">Reset password</a>`
+                });
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+    })
 };
 
 exports.postLogin = (req, res, next) => {
@@ -103,3 +154,24 @@ exports.postLogout = (req, res, next) => {
     })
     .catch(error => { console.log(error) });
 };
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    console.log(token)
+    User.findOne({resetToken: token, resetTokenExp: {$gt: Date.now()}})
+    .then((user)=>{
+        console.log(user)
+        if(!user){
+            return res.redirect('/login');
+        }
+        res.render("shop/new-password", {
+            docTitle: "New password",
+            path: "/reset-password",
+            userId: user._id.toString()
+        });
+
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,12 @@ const router = express.Router();
 
 router.get('/signup', authController.getSignup);
 router.get('/login', authController.getLogin);
+router.get('/reset', authController.getReset);
+router.get('/reset-password/:token', authController.getNewPassword);
 
 router.post('/login', authController.postLogin);
 router.post('/signup', authController.postSignup);
 router.post('/logout', authController.postLogout);
+router.post('/reset', authController.postReset);
 
 module.exports = router;
