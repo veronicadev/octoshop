646ab4033ac00fc674bd4c844b8750b74ef2bcf9
veronicadev <viarengoveronica@gmail.com>
2019-10-28 17:16:38 +0100

    handle user role

--- util/roles.js
+++ util/roles.js
@@ -1,3 +1,5 @@
+
+const Role = require('./../models/role');
 const roles = {
     ADMIN: "admin",
     CUSTOMER: "customer"
@@ -8,5 +10,26 @@ const rolesRedirect = {
     CUSTOMER: "/customer/dashboard"
 };
 
+const canUser = (action, type, userRole) => {
+    Role.find()
+        .then(roles => {
+                const indexRole = roles.findIndex(r=>{
+                    return r.name===userRole
+                })
+                if(indexRole!==-1){
+                    console.log(roles[indexRole].perms[type].indexOf(action)!== -1 && roles[indexRole].perms[type].length>0)
+                    if (roles[indexRole].perms[type].length>0 && roles[indexRole].perms[type].indexOf(action)!== -1) {
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+        })
+        .catch(error => {
+            return false;
+        })
+}
+
 exports.roles = roles;
 exports.rolesRedirect = rolesRedirect;
+exports. canUser = canUser;
