5d7e94d22b793a6270e2c1b4177f3c420a58fd94
veronicadev <viarengoveronica@gmail.com>
2019-10-29 12:15:02 +0100

    error view

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,13 +1,27 @@
 exports.get404 = (req, res, next)=>{
-    res.status(404).render("404", {
+    res.status(404).render("errors/404", {
         docTitle: "404",
         path:'/404'
     });
 }
 
 exports.get500 = (req, res, next)=>{
-    res.status(500).render("500", {
+    res.status(500).render("errors/500", {
         docTitle: "500",
         path:'/500'
+    });
+}
+
+exports.get403 = (req, res, next)=>{
+    res.status(403).render("errors/403", {
+        docTitle: "403",
+        path:'/403'
+    });
+}
+
+exports.get401 = (req, res, next)=>{
+    res.status(401).render("errors/401", {
+        docTitle: "401",
+        path:'/401'
     });
 }
